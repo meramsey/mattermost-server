29df1de1d1216004e885c3cdfd97a0b13b8aaef8
coltoneshaw <colton@theshaw.group>
2023-03-23 14:15:26 -0400

    all channels tests passing

--- webapp/channels/src/selectors/admin_console.test.jsx
+++ webapp/channels/src/selectors/admin_console.test.jsx
@@ -1,7 +1,7 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import AdminDefinition from 'components/admin_console/admin_definition.jsx';
+import AdminDefinition from 'components/admin_console/admin_definition';
 
 import {getAdminDefinition} from 'selectors/admin_console.jsx';
 
